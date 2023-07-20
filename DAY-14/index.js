5a5886c24d7af45931b833f3329342ffb4a84e1b
Ajil <arafath2092@gmail.com>
2023-07-20 13:10:00 +0530

    DAY-15 TASK

---
+++ DAY-14/index.js
@@ -0,0 +1,18 @@
+const windowWidth = window.innerWidth;
+const windowHeight =window.innerHeight;
+
+//window.scrollBy(0, 200);
+//window.scrollTo(0, 0);
+
+//setInterval(() => {
+//   window.scrollBy(0,100);
+//},3000);
+
+//setTimeout(() => {
+//    window.scrollBy(0,500);
+//},3000);
+
+console.log("Window width:", windowWidth);
+console.log("Window height:", windowHeight);
+
+//console.log("Current URL:", window.location.href);
--- DAY-15/index.js
+++ DAY-15/index.js
@@ -1,12 +1,10 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
   const container = document.getElementById('pagination-container');
-  const totalPages = 10; // Total number of pages (you can set this dynamically based on your data)
-  const currentPage = 1; // Current active page (you can set this dynamically)
+  const totalPages = 10; 
+  const currentPage = 1; 
 
-  // Function to generate pagination links
   function generatePaginationLinks(totalPages, currentPage) {
-    container.innerHTML = ''; // Clear any previous links
+    container.innerHTML = ''; 
 
     for (let page = 1; page <= totalPages; page++) {
       const link = document.createElement('a');
@@ -22,10 +20,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  // Call the function to generate pagination links
   generatePaginationLinks(totalPages, currentPage);
 
-  // Add click event listener to handle pagination click
   container.addEventListener('click', function(event) {
     event.preventDefault();
 
